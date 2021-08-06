4a8cd32db8158cf85e9a637ac5cd222604fde07a
atournayre <a.tournayre@aroban.com>
2021-08-06 22:37:26 +0200

    Mise en place de l'entité utilisateur + fixtures

---
+++ api/src/DataFixtures/UtilisateurFixture.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Utilisateur;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class UtilisateurFixture extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $admin = new Utilisateur();
+        $admin->setEmail('support@aroban.com');
+        $admin->setNom('Aroban');
+        $admin->setPrenom('Admin');
+        $admin->setRoles(['ROLE_USER']);
+        $manager->persist($admin);
+
+        $manager->flush();
+    }
+}
