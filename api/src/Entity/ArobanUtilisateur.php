aaefd796cea2ebe1201e2eee70c3d3893289d5ed
atournayre <aurelien.tournayre@gmail.com>
2021-08-09 22:37:42 +0200

    Mise en place de l'entité ArobanUtilisateur + annotations API Platform + règles de validation + remplacement de id par UUID + suppression des dépréciations Symfony + méthodes métier.

--- api/src/Entity/ArobanUtilisateur.php
+++ api/src/Entity/ArobanUtilisateur.php
@@ -2,48 +2,56 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
+use JetBrains\PhpStorm\Pure;
+use Symfony\Component\Security\Core\User\LegacyPasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
-/**
- * TODO Supprimer cette ligne dans le bundle
- * @ORM\Entity
- */
-#[ApiResource()]
-class Utilisateur implements UserInterface
+class ArobanUtilisateur implements UserInterface, PasswordAuthenticatedUserInterface, LegacyPasswordAuthenticatedUserInterface
 {
     /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
+     * @ORM\Id)
+     * @ORM\GeneratedValue(strategy="UUID")
      * @ORM\Column(type="integer")
      */
-    private ?int $id = null;
+    #[Groups(['user:read'])]
+    protected ?int $uuid = null;
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
+     * @Assert\NotBlank(message="L'email de l'utilisateur est obligatoire.")
+     * @Assert\Email(message="L'email est invalide.")
+     * @Assert\Unique(message="L'email est déjà utilisé.")
      */
-    private string $email;
+    #[Groups(['user:read', 'user:write'])]
+    protected string $email;
 
     /**
      * @ORM\Column(type="json")
      */
-    private array $roles = [];
+    #[Groups(['user:read', 'admin:write'])]
+    protected array $roles = [];
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Le nom de l'utilisateur est obligatoire.")
      */
-    private string $nom;
+    #[Groups(['user:read', 'user:write'])]
+    protected string $nom;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Le prénom de l'utilisateur est obligatoire.")
      */
-    private string $prenom;
+    #[Groups(['user:read', 'user:write'])]
+    protected string $prenom;
 
-    public function getId(): ?int
+    public function getUuid(): ?int
     {
-        return $this->id;
+        return $this->uuid;
     }
 
     public function getEmail(): ?string
@@ -58,27 +66,22 @@ class Utilisateur implements UserInterface
         return $this;
     }
 
-    /**
-     * A visual identifier that represents this user.
-     *
-     * @see UserInterface
-     */
     public function getUserIdentifier(): string
     {
         return (string) $this->email;
     }
 
     /**
+     * This method can be removed in Symfony 6.0
+     *
      * @deprecated since Symfony 5.3, use getUserIdentifier instead
      */
+    #[Pure]
     public function getUsername(): string
     {
-        return (string) $this->email;
+        return$this->getUserIdentifier();
     }
 
-    /**
-     * @see UserInterface
-     */
     public function getRoles(): array
     {
         $roles = $this->roles;
@@ -95,29 +98,16 @@ class Utilisateur implements UserInterface
         return $this;
     }
 
-    /**
-     * This method can be removed in Symfony 6.0 - is not needed for apps that do not check user passwords.
-     *
-     * @see PasswordAuthenticatedUserInterface
-     */
     public function getPassword(): ?string
     {
         return null;
     }
 
-    /**
-     * This method can be removed in Symfony 6.0 - is not needed for apps that do not check user passwords.
-     *
-     * @see UserInterface
-     */
     public function getSalt(): ?string
     {
         return null;
     }
 
-    /**
-     * @see UserInterface
-     */
     public function eraseCredentials()
     {
         // If you store any temporary, sensitive data on the user, clear it here
@@ -146,5 +136,15 @@ class Utilisateur implements UserInterface
         $this->prenom = $prenom;
 
         return $this;
+    }
+
+    public function getNomPrenom($separateur = null): string
+    {
+        return implode($separateur ?? ' ', [$this->nom, $this->prenom]);
+    }
+
+    public function getPrenomNom($separateur = null): string
+    {
+        return implode($separateur ?? ' ', [$this->prenom, $this->nom]);
     }
 }
