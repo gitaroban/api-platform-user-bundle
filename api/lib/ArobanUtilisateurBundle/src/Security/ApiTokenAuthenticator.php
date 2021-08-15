9d3ac4e5d6567597caa0f393daa56cde729a4f10
atournayre <a.tournayre@aroban.com>
2021-08-15 16:24:14 +0200

    Modification du message en cas d'erreur d'authentification par token pour le bundle.

--- api/lib/ArobanUtilisateurBundle/src/Security/ApiTokenAuthenticator.php
+++ api/lib/ArobanUtilisateurBundle/src/Security/ApiTokenAuthenticator.php
@@ -54,21 +54,10 @@ class ApiTokenAuthenticator extends AbstractAuthenticator
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
     {
-        $data = [
-            'message' => strtr($exception->getMessageKey(), $exception->getMessageData()),
-        ];
-
-        return new JsonResponse($data, Response::HTTP_UNAUTHORIZED);
+        return new JsonResponse([
+                'message' => 'Invalid credentials',
+            ],
+            Response::HTTP_UNAUTHORIZED
+        );
     }
-
-//    public function start(Request $request, AuthenticationException $authException = null): Response
-//    {
-//        /*
-//         * If you would like this class to control what happens when an anonymous user accesses a
-//         * protected page (e.g. redirect to /login), uncomment this method and make this class
-//         * implement Symfony\Component\Security\Http\EntryPoint\AuthenticationEntrypointInterface.
-//         *
-//         * For more details, see https://symfony.com/doc/current/security/experimental_authenticators.html#configuring-the-authentication-entry-point
-//         */
-//    }
 }
