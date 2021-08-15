d2521f36a850398fdde5ce44a048be0307f739ef
atournayre <a.tournayre@aroban.com>
2021-08-15 16:21:13 +0200

    Mise en place de ApiTokenAuthenticator dans l'application.

--- api/src/Repository/UtilisateurRepository.php
+++ api/src/Repository/UtilisateurRepository.php
@@ -2,8 +2,11 @@
 
 namespace App\Repository;
 
+use App\Entity\ApiToken;
 use App\Entity\Utilisateur;
+use Aroban\Bundle\UtilisateurBundle\Repository\ArobanUtilisateurRepositoryInterface;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Query\Expr\Join;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -12,10 +15,24 @@ use Doctrine\Persistence\ManagerRegistry;
  * @method Utilisateur[]    findAll()
  * @method Utilisateur[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class UtilisateurRepository extends ServiceEntityRepository
+class UtilisateurRepository extends ServiceEntityRepository implements ArobanUtilisateurRepositoryInterface
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Utilisateur::class);
+    }
+
+    public function fetchByToken(string $token): ?Utilisateur
+    {
+        return $this->createQueryBuilder('u')
+            ->select('u')
+            ->join(ApiToken::class, 'at', Join::WITH, 'at.utilisateur = u')
+            ->andWhere('at.token = :token')
+            ->andWhere('at.expiresAt >= :expiresAt')
+            ->setParameter('token', $token)
+            ->setParameter('expiresAt', new \DateTime())
+            ->setMaxResults(1)
+            ->getQuery()
+            ->getOneOrNullResult();
     }
 }
